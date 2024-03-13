afdf60dac86354feb667d1d855a4d0ad19e6de84
Sayali <Sayali@LAPTOP-94G6IOLH>
2024-03-13 13:23:27 +0530

    [sayali] package sample2 created

--- src/sample1/Ex3.java
+++ src/sample1/Ex3.java
@@ -1,5 +1,10 @@
 package sample1;
 
-public class Ex3 {
-
+public class Ex3
+{
+  public static void main(String[] args)
+  {
+	  System.out.println("sayali wed jayesh");
+	
+}
 }
---
+++ src/sample2/Test1.java
@@ -0,0 +1,11 @@
+package sample2;
+
+public class Test1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("test case 1");
+		
+	}
+
+}
